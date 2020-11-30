ccc72e5c99e5ca2fb747df11f2e5dfcd52c6b9ad
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 02:03:29 +0530

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Main from './Main';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Activate from './screens/Activate';
+import Login from './screens/Login';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route path='/' exact render={props => <Main {...props} />} />
         <Route path='/register' exact render={props => <Register {...props} />} />
+        <Route path='/login' exact render={props => <Login {...props} />} />
         <Route path='/users/activate/:token' exact render={props => <Activate {...props} />} />
       </Switch>
     </BrowserRouter>
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import authSvg from '../assets/auth.svg';
+import authSvg from '../assets/login.svg';
 import {ToastContainer, toast} from 'react-toastify';
 import {authenticate, isAuth} from '../helpers/auth';
 import jwt from 'jsonwebtoken';
@@ -7,39 +7,33 @@ import axios from 'axios';
 import {Link, Redirect} from 'react-router-dom';
 
 
-const Register = () => {
+const Login = () => {
     const [formData,setFormData] = useState({
-        name: '',
         email: '',
         password1: '',
-        password2: ''
     })
 
-    const {name, email, password1, password2} = formData;
+    const {email, password1} = formData;
     const handleChange = text => e =>  {
-        console.log(name,email, password1, password2);
+        console.log(email, password1);
         setFormData({...formData, [text]: e.target.value})
     }
 
     const handleSubmit = e => {
+        console.log(process.env.REACT_APP_API_URL);
         e.preventDefault();
-        if(name && email && password1 ) {
-            if(password1 === password2){
-                axios.post(`${process.env.REACT_APP_API_URL}/register`, {
-                    name,email, password: password1
+        if( email && password1 ) {
+                axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+                    email, password: password1
                 }).then(res => {
-                    setFormData({...formData, name:'',email:'', password1:'',password2: ''})
-                    toast.success(res.data.message);
+                    setFormData({...formData, email:'', password1:''})
+                    console.log(res.data);
+                    toast.success('Sign in Successful');
                 }).catch(err => {
                     toast.error(err.response.data.error);
                 })
-
-                
-            } else {
-                toast.error('Passwords dont match')
-            }
         } else {
-            toast.error('Please fill al fields')
+            toast.error('Please fill all fields')
         }
     }
     return(
@@ -48,22 +42,16 @@ const Register = () => {
             <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
                 <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
                     <div className='mt-12 flex flex-col items-center'>
-                        <h1 className='text-2xl xl:text-3xl font-extrabold'>Sign up for FashionPoint</h1>
+                        <h1 className='text-2xl xl:text-3xl font-extrabold'>Sign In for FashionPoint</h1>
                         <form className='w-full flex-1 mt-8 text-indigo-500'
                             onSubmit={handleSubmit}
                             >
                             <div className='mx-auto max-w-xs relative'>
-                                <input type='text' 
-                                       placeholder='Name'
-                                       onChange={handleChange('name')}
-                                       value={name}
-                                       className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
-                                       />
                                 <input type='email' 
                                        placeholder='Email'
                                        onChange={handleChange('email')}
                                        value={email}
-                                       className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                                       className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
                                        />
                                 <input type='password' 
                                        placeholder='Password'
@@ -71,28 +59,22 @@ const Register = () => {
                                        value={password1}
                                        className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
                                        />
-                                <input type='password' 
-                                       placeholder='Confirm Password'
-                                       onChange={handleChange('password2')}
-                                       value={password2}
-                                       className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
-                                       />
-                                <button type='submit' className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'>Register</button>
+                                <button type='submit' className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'>Login</button>
 
                             </div>
                             <div className='my-12 border-b text-center'>
                                 <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                                    Or Sign in With email or social login
+                                    Or Sigup in With email or social login
                                 </div>
                             </div>
                             <div className='flex flex-col items-center'>
                             <a
                                     className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
                                                bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
-                                    href='/login'
+                                    href='/register'
                                     target='_self'
                                     >
-                                       Sign In
+                                       Sign Up
                                     </a>
                             </div>
                         </form>
@@ -110,4 +92,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default Login;
