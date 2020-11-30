23e93068f64dd5b35e616823778f8dc835e473d8
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-11-30 17:16:24 +0530

    Client URL

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+
+require('dotenv').config({
+    path:'./config/config.env'
+})
 const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: process.env.client_URL
+    }))
+}
 
 const PORT = process.env.PORT
 
 app.listen(PORT, ()=> {
     console.log(`App listening on port ${PORT}`);
 })
+
