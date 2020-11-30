32d17030e74449191c1e71dd6948b20cca113244
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-11-30 23:02:04 +0530

    register

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {BrowserRouter,Route, Redirect, Switch} from 'react-router-dom';
 import Register from './screens/Register';
 import Main from './Main';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
     <BrowserRouter>
+    <ToastContainer />
       <Switch>
         <Route path='/' exact render={props => <Main {...props} />} />
         <Route path='/register' exact render={props => <Register {...props} />} />
--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-//import authSvg from '../assets/auth.svg';
+import authSvg from '../assets/auth.svg';
 import {ToastContainer, toast} from 'react-toastify';
 import {authenticate, isAuth} from '../helpers/auth';
 import axios from 'axios';
@@ -11,21 +11,22 @@ const Register = () => {
     const [formData,setFormData] = useState({
         name: '',
         email: '',
-        passowrd1: '',
+        password1: '',
         password2: ''
     })
 
-    const {name, email, passowrd1, password2} = formData;
-    const handleChange = test = e =>  {
+    const {name, email, password1, password2} = formData;
+    const handleChange = text => e =>  {
+        console.log(name,email, password1, password2);
         setFormData({...formData, [text]: e.target.value})
     }
 
     const handleSubmit = e => {
         e.preventDefault();
-        if(name && email && passowrd1 ) {
+        if(name && email && password1 ) {
             if(password1 === password2){
                 axios.post(`${process.env.REACT_APP_API_URL}/register`, {
-                    name,email, password: passowrd1
+                    name,email, password: password1
                 }).then(res => {
                     setFormData({...formData, name:'',email:'', password1:'',password2: ''})
                     toast.success(res.data.message);
@@ -48,6 +49,60 @@ const Register = () => {
                 <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
                     <div className='mt-12 flex flex-col items-center'>
                         <h1 className='text-2xl xl:text-3xl font-extrabold'>Sign up for FashionPoint</h1>
+                        <form className='w-full flex-1 mt-8 text-indigo-500'
+                            onSubmit={handleSubmit}
+                            >
+                            <div className='mx-auto max-w-xs relative'>
+                                <input type='text' 
+                                       placeholder='Name'
+                                       onChange={handleChange('name')}
+                                       value={name}
+                                       className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                                       />
+                                <input type='email' 
+                                       placeholder='Email'
+                                       onChange={handleChange('email')}
+                                       value={email}
+                                       className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                                       />
+                                <input type='password' 
+                                       placeholder='Password'
+                                       onChange={handleChange('password1')}
+                                       value={password1}
+                                       className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                                       />
+                                <input type='password' 
+                                       placeholder='Confirm Password'
+                                       onChange={handleChange('password2')}
+                                       value={password2}
+                                       className='mt-3 w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                                       />
+                                <button type='submit' className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'>Register</button>
+
+                            </div>
+                            <div className='my-12 border-b text-center'>
+                                <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                                    Or Sign in With email or social login
+                                </div>
+                            </div>
+                            <div className='flex flex-col items-center'>
+                            <a
+                                    className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
+                                               bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                                    href='/login'
+                                    target='_self'
+                                    >
+                                       Sign In
+                                    </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className='flex-1 bg-indigo-100 text-center hidden lg:flex'>
+                    <div className='m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat'
+                        style={{backgroundImage: `url(${authSvg})`}}
+                        >
+
                     </div>
                 </div>
             </div>
