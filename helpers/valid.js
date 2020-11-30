a5dee4a96b8a9021eb2baf3a08bab61a2fd7fbc9
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 00:14:38 +0530

    valid.js

---
+++ helpers/valid.js
@@ -0,0 +1,33 @@
+const { removeLocalStorage } = require("../client/src/helpers/auth");
+
+const {check} = require('express-validator');
+
+exports.validRegister = [
+    check('name','Name is Required').isEmpty().isLength({
+        min: 3,
+        max: 32
+    }).withMessage('name must bebetween 3 to 32 characters'),
+    check('email').isEmpty().withMessage('Must be a valid email address'),
+    check('password','Password is Required').notEmpty(),
+    check('password').isLength({
+        min: 8,
+    }).withMessage('Password mus contain at least 8 characters').matches(/\d/).withMessage('Password must contain a number')
+
+]
+
+exports.validLogin = [
+    check('email').isEmail().withMessage('Must be a valid email address'),
+    check('password','Password is Required').notEmpty(),
+    check('password').isLength({
+        min: 8,
+    }).withMessage('Password mus contain at least 8 characters').matches(/\d/).withMessage('Password must contain a number')
+
+]
+
+
+exports.resetPasswordValidator = [
+    check('newPassword').isLength({
+        min: 8,
+    }).withMessage('Password mus contain at least 8 characters').matches(/\d/).withMessage('Password must contain a number')
+
+]
