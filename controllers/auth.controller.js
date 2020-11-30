69e33dc98e8f881d922295b469c4c89b0ab8ecec
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-11-30 19:37:39 +0530

    DB Error Handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,13 @@
+const User = require('../models/auth.model');
+const expressJwt = require('express-jwt');
+const _ = require('lodash');
+const {OAuth2Client} = require('google-auth-library');
+const fetch = require('node-fetch');
+const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {errorHandler} = require('../helpers/dbErrorHandling');
+
+ 
 exports.registerController = (req,res) => {
     const {name,email,password} = req.body;
     console.log(name,email,password); 
---
+++ helpers/dbErrorHandling.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const uniqueMessage = error => {
+    let output;
+    try {
+        let fieldName = error.message.split(".$")[1];
+        field = field.split(" dub key")[0]
+        field = field.substring(0,field.lastIndexOf("_"));
+        req.flash("errors",[{
+            message: "An Account with this " + field + "already exists"
+        }])
+        output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + 'already exists' 
+    } catch(error) {
+        output = "already exists"
+    }
+
+    return output;
+
+}
+
+exports.errorHandler = error => {
+    let message = ""
+    if(error.code) {
+        switch(error.code){
+            case 11000:
+            case 11001:
+                message = uniqueMessage(error);
+                break;
+            default:
+                message = "Something Went Wrong"
+        }
+    }  else {
+        for(let errorName in error.errorors){
+            if(error.errorors[errorName].message){
+                message = error.errorors[errorName].message
+            }
+        }
+    }
+    return message
+}
