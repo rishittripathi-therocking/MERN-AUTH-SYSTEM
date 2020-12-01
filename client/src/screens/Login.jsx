975ab908efa7916617c310c14e550da647bf11a4
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 15:06:44 +0530

    SignIn/Sigup google oauth

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -122,15 +122,34 @@ const Login = ({history}) => {
                                 </div>
                             </div>
                             <div className='flex flex-col items-center'>
-                            <a
-                                    className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
-                                               bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
-                                    href='/register'
-                                    target='_self'
-                                    >
-                                       <i className='fas fa-user-plus fa 1x w-6  -ml-2 text-indigo-500' />
-                                        <span className='ml-4'>Sign Up</span>
-                                    </a>
+                              <GoogleLogin
+                                        clientId={`${process.env.REACT_APP_GOOGLE_CLIENT}`}
+                                        onSuccess={responseGoogle}
+                                        onFailure={responseGoogle}
+                                        cookiePolicy={'single_host_origin'}
+                                        render={renderProps => (
+                                          <button
+                                            onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}
+                                            className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline'
+                                          >
+                                            <div className=' p-2 rounded-full '>
+                                              <i className='fab fa-google ' />
+                                            </div>
+                                            <span className='ml-4'>Sign In with Google</span>
+                                          </button>
+                                        )}
+                                      ></GoogleLogin>
+                              <a
+                                      className='mt-3 w-full max-w-xs font-bold shadow-sm rounded-lg py-3
+                                                bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                                      href='/register'
+                                      target='_self'
+                                      >
+                                        <i className='fas fa-user-plus fa 1x w-6  -ml-2 text-indigo-500' />
+                                          <span className='ml-4'>Sign Up</span>
+                                      </a>
+                              
                             </div>
                         </form>
                     </div>
