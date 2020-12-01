91e798f0cc13212439f346bdd6eb33bdda99e6d3
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 15:27:07 +0530

    SignIn/Sigup Facebook oauth button backend

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -351,3 +351,58 @@ exports.googleController = (req, res) => {
         }
       });
 };
+
+exports.facebookController = (req, res) => {
+  console.log('FACEBOOK LOGIN REQ BODY', req.body);
+  const { userID, accessToken } = req.body;
+
+  const url = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`;
+
+  return (
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(response => {
+        const { email, name } = response;
+        User.findOne({ email }).exec((err, user) => {
+          if (user) {
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+              expiresIn: '7d'
+            });
+            const { _id, email, name, role } = user;
+            return res.json({
+              token,
+              user: { _id, email, name, role }
+            });
+          } else {
+            let password = email + process.env.JWT_SECRET;
+            user = new User({ name, email, password });
+            user.save((err, data) => {
+              if (err) {
+                console.log('ERROR FACEBOOK LOGIN ON USER SAVE', err);
+                return res.status(400).json({
+                  error: 'User signup failed with facebook'
+                });
+              }
+              const token = jwt.sign(
+                { _id: data._id },
+                process.env.JWT_SECRET,
+                { expiresIn: '7d' }
+              );
+              const { _id, email, name, role } = data;
+              return res.json({
+                token,
+                user: { _id, email, name, role }
+              });
+            });
+          }
+        });
+      })
+      .catch(error => {
+        res.json({
+          error: 'Facebook login failed. Try later'
+        });
+      })
+  );
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {validRegister, validLogin,resetPasswordValidator, forgotPasswordValidator} = require('../helpers/valid');
-const {activationController, registerController, loginController, forgotPasswordController, resetPasswordController, googleController} = require('../controllers/auth.controller');
+const {activationController, registerController, loginController, forgotPasswordController, resetPasswordController, googleController, facebookController} = require('../controllers/auth.controller');
 
 router.post('/register',validRegister,registerController);
 router.post('/login',validLogin,loginController);
 router.post('/activation',activationController);
 router.put('/password/forget',forgotPasswordValidator, forgotPasswordController);
 router.put('/password/reset',resetPasswordValidator, resetPasswordController);
-router.post('/googlelogin', googleController)
+router.post('/googlelogin', googleController);
+router.post('/facebooklogin', facebookController)
 
 module.exports = router
