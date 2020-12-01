250a2796a9f1e5f27ccb4c8731d7d6c1952720bb
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 16:10:36 +0530

    Auth Controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -301,7 +301,30 @@ exports.resetPasswordController = (req, res) => {
     }
   };
 
-
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET // req.user._id
+});
+  
+exports.adminMiddleware = (req, res, next) => {
+    User.findById({
+      _id: req.user._id
+    }).exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: 'User not found'
+        });
+      }
+  
+      if (user.role !== 'admin') {
+        return res.status(400).json({
+          error: 'Admin resource. Access denied.'
+        });
+      }
+  
+      req.profile = user;
+      next();
+    });
+};
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT);
 exports.googleController = (req, res) => {
