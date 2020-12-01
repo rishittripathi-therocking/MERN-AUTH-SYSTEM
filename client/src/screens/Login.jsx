afac9b746a838b1aecc7d2fb265df77921b5f325
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 16:06:15 +0530

    Route on all Screens

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -188,6 +188,17 @@ const Login = ({history}) => {
                                       </a>
                               
                             </div>
+                            <div className='flex flex-col items-center'>
+                              <a
+                                className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
+                                      bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                                href='/'
+                                target='_self'
+                              >
+                                <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
+                                <span className='ml-4'>Home</span>
+                              </a>
+                            </div>
                         </form>
                     </div>
                 </div>
--- client/src/screens/ResetPassword.jsx
+++ client/src/screens/ResetPassword.jsx
@@ -86,6 +86,17 @@ const ResetPassword = ({match}) => {
                   <i className='fas fa-sign-in-alt  w-6  -ml-2' />
                   <span className='ml-3'>Submit</span>
                 </button>
+                <div className='flex flex-col items-center'>
+                  <a
+                    className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
+                          bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+                    href='/'
+                    target='_self'
+                  >
+                    <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
+                    <span className='ml-4'>Home</span>
+                  </a>
+                </div>
               </form>
             </div>
           </div>
