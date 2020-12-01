3c96184fc0bd7a254a6edf772d64c072da244b39
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-12-01 14:48:46 +0530

    SignIn/Sigup google oauth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -300,3 +300,54 @@ exports.resetPasswordController = (req, res) => {
       }
     }
   };
+
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT);
+exports.googleController = (req, res) => {
+    const { idToken } = req.body;
+  
+    client
+      .verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT })
+      .then(response => {
+        const { email_verified, name, email } = response.payload;
+        if (email_verified) {
+          User.findOne({ email }).exec((err, user) => {
+            if (user) {
+              const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: '7d'
+              });
+              const { _id, email, name, role } = user;
+              return res.json({
+                token,
+                user: { _id, email, name, role }
+              });
+            } else {
+              let password = email + process.env.JWT_SECRET;
+              user = new User({ name, email, password });
+              user.save((err, data) => {
+                if (err) {
+                  console.log('ERROR GOOGLE LOGIN ON USER SAVE', err);
+                  return res.status(400).json({
+                    error: 'User signup failed with google'
+                  });
+                }
+                const token = jwt.sign(
+                  { _id: data._id },
+                  process.env.JWT_SECRET,
+                  { expiresIn: '7d' }
+                );
+                const { _id, email, name, role } = data;
+                return res.json({
+                  token,
+                  user: { _id, email, name, role }
+                });
+              });
+            }
+          });
+        } else {
+          return res.status(400).json({
+            error: 'Google login failed. Try again'
+          });
+        }
+      });
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {validRegister, validLogin,resetPasswordValidator, forgotPasswordValidator} = require('../helpers/valid');
-const {activationController, registerController, loginController, forgotPasswordController, resetPasswordController} = require('../controllers/auth.controller');
+const {activationController, registerController, loginController, forgotPasswordController, resetPasswordController, googleController} = require('../controllers/auth.controller');
 
 router.post('/register',validRegister,registerController);
 router.post('/login',validLogin,loginController);
 router.post('/activation',activationController);
 router.put('/password/forget',forgotPasswordValidator, forgotPasswordController);
 router.put('/password/reset',resetPasswordValidator, resetPasswordController);
+router.post('/googlelogin', googleController)
 
 module.exports = router
